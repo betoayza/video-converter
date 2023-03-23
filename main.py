f9fbdc80e8ef9e93d4855997eb82a5705598cb01
betoayza <aaayza@gmail.com>
2023-03-23 14:25:40 -0300

    First commit

--- main.py
+++ main.py
@@ -6,9 +6,8 @@ initial_format = video_path[video_path.index(".")+1:]
 dicc = {1: "avi", 2: "mp4", 3: "mov", 4: "wmv", 5: "mkv", 6: "webm"}
 is_finished = False
 
-print("Welcome to the Video Converter!\n")
+print("\nWelcome to the Video Converter!\n\nThe initial format is:", initial_format)
 
-print("The initial format is:", initial_format)
 while not is_finished:
     try:
         chosen_format = int(input("""
@@ -22,14 +21,14 @@ while not is_finished:
             6.    WEBM
 
             Select: """))
-        
+
         if initial_format == dicc[chosen_format]:
             print("\nCan't choice same format!")
             continue
 
         clip.write_videofile(
             f"result/brujula.{dicc[chosen_format]}", codec='libx264')
-        
+
         is_finished = True
     except ValueError:
         print("\nYou have to choice a valid option!")
