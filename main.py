cec5e7f06a71979b56d4600b0ce9a337a573e287
betoayza <aaayza@gmail.com>
2023-03-25 18:35:20 -0300

    GUI updated

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from moviepy.editor import VideoFileClip
 import tkinter as tk
-import sys, os
+import sys
+import os
 
 result = ""
 
@@ -8,66 +9,81 @@ result = ""
 def convert_video(chosen_format):
     global result
 
-    current_dir = os.path.dirname(__file__)
-    print("ASD:", chosen_format)
+    current_dir = os.path.dirname(__file__)  
 
     video_path = current_dir + "/video/brujula.mp4"
     video_name = video_path[video_path.rindex("/")+1:video_path.rindex(".")]
     clip = VideoFileClip(video_path)
     initial_format = video_path[video_path.index(".")+1:]
     dicc = {1: "avi", 2: "mp4", 3: "mov", 4: "wmv", 5: "mkv", 6: "webm"}
-    is_finished = False
 
     try:
         chosen_format = int(chosen_format)
 
-        if chosen_format == 7:
-                clip.write_gif(f"{current_dir}/result/gif/{video_name}.gif")
-
-        elif initial_format == dicc[chosen_format]:
+        if initial_format == dicc[chosen_format]:
             result = "Can't choice same format!"
-
+            label_result.config(text=result)
         else:
-             clip.write_videofile(
-                f"{current_dir}/result/video/{video_name}.{dicc[chosen_format]}", codec='libx264')
+            # result = "Converting..."
+            # label_result.config(text=result)
 
-             result = "Successful!"
+            if chosen_format == 7:
+                clip.write_gif(f"{current_dir}/result/gif/{video_name}.gif")
+            else:
+                clip.write_videofile(
+                    f"{current_dir}/result/video/{video_name}.{dicc[chosen_format]}", codec='libx264')
+
+            result = "Successful!"
+            label_result.config(text=result)
+
     except ValueError:
         result = "Error: you have to choice a valid option!"
+        label_result.config(text=result)
 
+# tkinter: la parte grafica
+root = tk.Tk()
+root.title("Super Video Converter")
 
+frame = tk.Frame(root, bd=2, bg="#c9c0bb")
+frame.pack()
 
-# la parte grafica
-screen = tk.Tk()
-screen.title("Super Video Converter")
-screen.geometry("600x480")
-label_title = tk.Label(screen, text="Welcome to Super Video Converter!")
+label_title = tk.Label(frame, text="Welcome to Super Video Converter!", font=(
+    "Arial", 18), bg="#c4aead")
 label_title.pack()
-text_options = """
+label_options = '''
     Choose format to convert:
-    ------------------
-        1.    AVI
-        2.    MP4
-        3.    MOV
-        4.    WMV
-        5.    MKV
-        6.    WEBM
-        7.    GIF
+    -------------------------
+    1.    AVI
+    2.    MP4
+    3.    MOV
+    4.    WMV
+    5.    MKV
+    6.    WEBM
+    7.    GIF
 
-        Select:"""
-label_options = tk.Label(screen, text=text_options)
-label_options.pack()
-chose_option = tk.Text(screen, width=7, height=2)
-chose_option.insert("1.0", "")
-chose_option.pack()
-btn_convert = tk.Button(screen, text="Convert!",
-                        command=lambda: convert_video(chose_option.get("1.0", tk.END)))
-btn_convert.pack()
-label_result = tk.Label(screen, text=result)
-btn_exit=tk.Button(screen, text = "Exit", command = screen.quit)
-btn_exit.pack()
+    Select:'''
+label_options = tk.Label(frame, text=label_options, bg="#bfc1c2")
+
+entry = tk.Entry(frame)
+entry.config(font=('Arial', 12), width=7, bg="#004953", fg="white")
+
+btn_convert = tk.Button(frame, text="Convert!", command=lambda: convert_video(
+    entry.get()))
+
+label_result = tk.Label(frame, text="", bg="black", fg="white")
+
+btn_exit = tk.Button(frame, text="Exit", command=root.quit)
+
+# ubicar elementos en grilla
+label_title.grid(row=0, columnspan=4)
+label_options.grid(row=1, columnspan=4)
+entry.grid(row=2, column=2, padx=0, pady=0)
+btn_convert.grid(row=3, column=2, padx=0, pady=0)
+label_result.grid(row=4, column=2, padx=0, pady=0)
+btn_exit.grid(row=5, column=3, padx=0)
+
 # mostrar ventana
-screen.mainloop()
-# exit program
+root.mainloop()
+# exit program4
 print("Thanks for try me, see on next time!")
 sys.exit()
